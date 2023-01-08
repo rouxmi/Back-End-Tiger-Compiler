8288175e81faad629b31ed2983d1bd0d405ab3af
Boucher <lucie.boucher@telecomnancy.eu>
2023-01-08 18:11:46 +0100

    restriction de controle semantique boucle for aux bornes entières en dur

--- src/controlesemantique/BoucleFor.java
+++ src/controlesemantique/BoucleFor.java
@@ -5,31 +5,41 @@ import tds.Table;
 
 public class BoucleFor {
     public static boolean CheckBorneMinInfBorneMax(Ast min, Ast max, Table tds){
-        String nameMin =min.getClass().getName().replace('\n', '\0');
-        String nameMax =max.getClass().getName().replace('\n', '\0');
-        if(nameMin.equals("ast.In") && nameMax.equals("ast.In")){
-            int minInt = ((In)min).in;
-            int maxInt = ((In)max).in;
-            if(minInt<=maxInt){
-                return true;
+        if(min instanceof In && max instanceof In){
+            String nameMin =min.getClass().getName().replace('\n', '\0');
+            String nameMax =max.getClass().getName().replace('\n', '\0');
+            if(nameMin.equals("ast.In") && nameMax.equals("ast.In")){
+                int minInt = ((In)min).in;
+                int maxInt = ((In)max).in;
+                if(minInt<=maxInt){
+                    return true;
+                }
             }
+            System.err.println("\u001B[91mBoucleForException dans "+tds.nom+" : Borne min supérieure à borne max (attendu une borne min strictement inférieure à une borne max)\u001B[0m\n");
+            return false;
         }
-        System.err.println("\u001B[91mBoucleForException dans "+tds.nom+" : Borne min supérieure à borne max (attendu une borne min strictement inférieure à une borne max)\u001B[0m\n");
-        return false;
+        else{
+            return true;
+        }
     }
 
     public static boolean CheckBorneMinNotBorneMax(Ast min, Ast max, Table tds) {
-        String nameMin =min.getClass().getName().replace('\n', '\0');
-        String nameMax =max.getClass().getName().replace('\n', '\0');
-        if(nameMin.equals("ast.In") && nameMax.equals("ast.In")){
-            int minInt = ((In)min).in;
-            int maxInt = ((In)max).in;
-            if(minInt!=maxInt){
-                return true;
+        if(min instanceof In && max instanceof In){
+            String nameMin =min.getClass().getName().replace('\n', '\0');
+            String nameMax =max.getClass().getName().replace('\n', '\0');
+            if(nameMin.equals("ast.In") && nameMax.equals("ast.In")){
+                int minInt = ((In)min).in;
+                int maxInt = ((In)max).in;
+                if(minInt!=maxInt){
+                    return true;
+                }
             }
+            System.err.println("\u001B[33mBoucleForWarning dans "+tds.nom+" : Borne min egale à borne max (attendu une borne min strictement inférieure à une borne max)\u001B[0m\n");
+            return false;
         }
-        System.err.println("\u001B[33mBoucleForWarning dans "+tds.nom+" : Borne min egale à borne max (attendu une borne min strictement inférieure à une borne max)\u001B[0m\n");
-        return false;
+        else{
+            return true;
+        }
     }
 
 }
